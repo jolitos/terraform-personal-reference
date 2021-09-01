eccb025622aa32e69a1efd1af12d04f0496754ee
João Vitor Costa Medeiros <jvcosmed@gmail.com>
2021-09-01 20:12:50 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket  = "tfstate-979089802435"
+    key     = "production/terraform.state"
+    region  = "us-east-2" # South America / São Paulo Region
+    profile = "terraform-ak"
+    encrypt = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "terraform-ak"
+}
---
+++ remote-state-s3/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 1.0.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "terraform-ak"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "remote-state" {
+  bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name      = "Terraform-State Bucket"
+    ManagedBy = "Terraform"
+    Owner     = "João Vitor C. Medeiros"
+  }
+}
+
+resource "aws_dynamodb_table" "lock-table" {
+  name           = "tflock-${aws_s3_bucket.remote-state.bucket}"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "remote_state_bucket" {
+  value = aws_s3_bucket.remote-state.bucket
+}
+
+output "remote_state_bucket_arn" {
+  value = aws_s3_bucket.remote-state.arn
+}
